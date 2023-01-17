301eedf909a1aec1a85d8598434c2221ae7624be
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-17 16:50:53 +0100

    Aggiunge query where all'interno del pageController

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $trains = Train::where('departure_time', '2023-01-17')->get();
+        return view('home', compact('trains'));
+    }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+
+@section('content')
+@foreach ($trains as $train)
+    <h1>{{$train -> company}}</h1>
+    
+@endforeach
+@endsection
